08a537c7dd0f720cb468fdc36802279a7e9b6677
Henrique Assis <henrique_assis_10@hotmail.com>
2020-09-16 19:34:36 -0300

    header teacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+//Routes
+import { Link } from 'react-router-dom'
+
+
+//ICONS
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+//CSS
+import './styles.css';
 
 function TeacherList(){
     return(
-        <h1>Teacher</h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="" />
+                    </Link>
+                    <img src={logoImg} alt="logo" />
+                </div>
+                <div className="header-content">
+                    <strong>Estes são os proffys disponíveis.</strong>
+                </div>
+            </header>
+        </div>
     )
 }
 
