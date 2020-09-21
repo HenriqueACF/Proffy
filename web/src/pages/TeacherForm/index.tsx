48cfc15b3d1f42d334f267b83200a88697cef78e
Henrique Assis <henrique_assis_10@hotmail.com>
2020-09-21 14:21:19 -0300

    edit pages

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 //CSS
 import './styles.css';
@@ -19,12 +21,51 @@ function TeacherForm(){
                 <Input name="name" label="Nome Completo" />
                 <Input name="avatar" label="Avatar" />
                 <Input name="whatsapp" label="WhatsApp" />
+                <Textarea name="bio" label="Biografia" />
             </fieldset>
 
             <fieldset>
                 <legend>Sobre a aula</legend>
-                <Input name="subject" label="Matéria" />
+                <Select 
+                    name="subject" 
+                    label="Matéria"
+                    options={[
+                        { value:'Artes', label: 'Artes' },
+                        { value:'Biologia', label: 'Biologia' },
+                        { value:'Ciências', label: 'Ciências' },
+                        { value:'Educação física', label: 'Educação física' },
+                        { value:'Geografia', label: 'Geografia' },
+                        { value:'História', label: 'História' },
+                        { value:'Matemática', label: 'Matemática' },
+                        { value:'Português', label: 'Português' },
+                        { value:'Química', label: 'Química' },
+                    ]}
+                 />
                 <Input name="cost" label="Custo da sua aula por hora" />
+            </fieldset>
+
+            <fieldset>
+                <legend>
+                    Horários disponíveis
+                    <button type="button">+ Novo horário</button>
+                    </legend>
+                    <div className="schedule-item">
+                    <Select 
+                    name="week_day" 
+                    label="Dia da semana"
+                    options={[
+                        { value:'0', label: 'Domingo' },
+                        { value:'1', label: 'Segunda-feira' },
+                        { value:'2', label: 'Terça-feira' },
+                        { value:'3', label: 'Quarta-feira' },
+                        { value:'4', label: 'Quinta-feira' },
+                        { value:'5', label: 'Sexta-feira' },
+                        { value:'6', label: 'Sabádo' },
+                    ]}
+                    />
+                    <Input name="from" label="Das" type="time"/>
+                    <Input name="to" label="Até" type="time" />
+                    </div>
             </fieldset>
 
             <footer>
